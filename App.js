1968f3ec5df36010deaedf3dd3bcd19c2a3251c5
Meenakshi Sundaram <meenakshisundaram@britisheducationonline.org>
2021-03-23 13:06:11 +0530

    Pull Down Refresh in List

---
+++ App.js
@@ -0,0 +1,105 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React,{useState} from 'react';
+ import {
+   RefreshControl,
+   ScrollView,
+   StyleSheet,
+   Text,
+   View,
+ } from 'react-native';
+ 
+ 
+   const App =  () => {
+    const [Items,setItems] = useState([
+      {
+        key:1,
+        item:'Item 1'
+      },
+      {
+        key:2,
+        item:'Item 2'
+      },
+      {
+        key:3,
+        item:'Item 3'
+      },
+      {
+        key:4,
+        item:'Item 4'
+      },
+      {
+        key:5,
+        item:'Item 5'
+      },
+      {
+        key:6,
+        item:'Item 6'
+      },
+      {
+        key:7,
+        item:'Item 7'
+      },
+      {
+        key:8,
+        item:'Item 8'
+      },
+    ])
+
+    const [Refreshing,setRefreshing] = useState(false)
+    const onRefresh = ()=>{
+        setRefreshing(true);
+        setItems([...Items,{key:69,item:'Item 69'}])
+        setRefreshing(false);
+    }
+  
+ 
+   return (     
+         <ScrollView style={styles.body}
+         refreshControl={
+           <RefreshControl
+           refreshing={Refreshing}
+           onRefresh={onRefresh}
+           colors={['#ff00ff']}/>
+         }>
+         {
+           Items.map((object)=>{
+             return(
+               <View style={styles.item} key={object.key}>
+                  <Text style={styles.text}>{object.item}</Text> 
+               </View>
+             )
+           })
+         }
+         </ScrollView>           
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   body: {
+     flex:1,
+     flexDirection: 'column',
+     backgroundColor: '#ffffff'
+   },
+   item: {
+     margin:10,
+     backgroundColor:'#4ae1fa',
+     alignItems:'center',
+     justifyContent:'center'  
+   },
+   text: {
+    color: '#000000',
+    fontSize: 45,
+    fontStyle: 'italic',
+    margin: 10,
+  }
+ });
+ 
+ export default App;
+ 
